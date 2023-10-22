07fff5fae641a8dcc914478ee7af46683d898b38
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-10-21 23:06:20 -0300

    data counter - criando a base

--- Data Counter/src/App.jsx
+++ Data Counter/src/App.jsx
@@ -1,8 +1,23 @@
+import "./style.css";
+
 function App() {
   return (
     <>
       <h1>Data Counter</h1>
+      <Counter />
     </>
+  );
+}
+
+function Counter() {
+  return (
+    <div>
+      <div className="buttons">
+        <button>-</button>
+        <span>Step: </span>
+        <button>+</button>
+      </div>
+    </div>
   );
 }
 
