b007c30f5b4e26956b1dd2b01f15338e58874d48
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-10-24 18:42:58 -0300

    criando o counter e aplicando a lógica

--- Data Counter/src/App.jsx
+++ Data Counter/src/App.jsx
@@ -12,10 +12,14 @@ function App() {
 
 function Counter() {
   const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
 
   function handleDecrease() {
     if (step >= 1) setStep(step - 1);
   }
+
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
 
   return (
     <div>
@@ -24,6 +28,22 @@ function Counter() {
         <span>Step: {step}</span>
         <button onClick={() => setStep((s) => s + 1)}>+</button>
       </div>
+
+      <div className="buttons">
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
     </div>
   );
 }
