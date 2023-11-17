74876da2b84fd8485b2a1efc5f2816661b596a3d
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-11-16 22:02:27 -0300

    commentary about how the conditional rendering used works

--- images/src/App.jsx
+++ images/src/App.jsx
@@ -23,19 +23,18 @@ function App() {
     <>
       <button onClick={handlePrevious}>Previous</button>
       <button onClick={handleNext}>Next</button>
-
       <h2>
         <i>{sculpture.name}</i> by {sculpture.artist}
       </h2>
-
       <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
       <button onClick={handleMoreClick}>
         {showMore ? "hide" : "Show"} details
       </button>
-      {showMore && <p>{sculpture.description}</p>}
 
+      {/* if showMore is true then show the p. */}
+      {showMore && <p>{sculpture.description}</p>}
       <img src={sculpture.url} alt={sculpture.alt} />
     </>
   );
